ad699c7dd02ed9ce5ab4709ee04fc9f898472a87
Karine Almeida <karinealmeida.kca@gmail.com>
2023-05-30 22:30:53 -0300

    Initial commit

---
+++ script_analise.R
@@ -0,0 +1,27 @@
+# Análise de dados AirBnB Buenos Aires argentina 
+
+pacotes <- c("plotly","tidyverse","ggrepel","fastDummies","knitr","kableExtra",
+             "splines","reshape2","PerformanceAnalytics","correlation","see",
+             "ggraph","psych","nortest","rgl","car","ggside","tidyquant","olsrr",
+             "jtools","ggstance","magick","cowplot","emojifont","beepr","Rcpp",
+             "equatiomatic")
+
+options(rgl.debug = TRUE)
+
+if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
+  instalador <- pacotes[!pacotes %in% installed.packages()]
+  for(i in 1:length(instalador)) {
+    install.packages(instalador, dependencies = T)
+    break()}
+  sapply(pacotes, require, character = T) 
+} else {
+  sapply(pacotes, require, character = T) 
+}
+
+# Carregando base de dados
+
+library(readr)
+calendar <- read_csv("calendar.csv")
+View(calendar)
+
+
